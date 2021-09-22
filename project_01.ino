1bf4cce19f2e61ab5638ee8825e86295e6c51631
PSU\akarapon-o <akarapon-o@psu.ac.th>
2021-09-22 13:01:08 +0700

    Update to compatible with new hardware

--- project_01.ino
+++ project_01.ino
@@ -7,42 +7,41 @@
 #include <Wire.h> 
 /* Servo */ 
 #include <Servo.h>
-
-
-/* Constants */
-#define KEYPAD_ROWS 4
-#define KEYPAD_COLS 4
+/* Define Parameters */
+/* Define PIN */
+#define DIR_PIN_RIGHT 3
+#define STEP_PIN_RIGHT 2
+#define DIR_PIN_LEFT 5
+#define STEP_PIN_LEFT 4
 #define RESET_BUT_PIN 12
 #define ANALOG_KEY_PIN A0
 #define BRUSH_SERVO_PIN 8
 #define UP_DOWN_SERVO_PIN 9
-#define DIR_PIN_RIGHT 2
-#define STEP_PIN_RIGHT 3
-#define DIR_PIN_LEFT 4
-#define STEP_PIN_LEFT 5
-#define STEPS_PER_MM 1
+/* Define Constants */
+#define KEYPAD_ROWS 4
+#define KEYPAD_COLS 4
+#define STEPS_PER_MM 100
 #define STEPS_PER_DEGREE 8
 #define DELAY 200
-
-
-/* Parameters do not change */
+/* Initial Parameters -- do not change */
 /* Keypad Parameters */
 const char kp4x4Keys[KEYPAD_ROWS][KEYPAD_COLS]  = {{'1', '2', '3', 'A'}, {'4', '5', '6', 'B'}, {'7', '8', '9', 'C'}, {'*', '0', '#', 'D'}};
 byte rowKp4x4Pin [4] = {26, 27, 28, 29};
 byte colKp4x4Pin [4] = {22, 23, 24, 25};
 /* LCD */
 byte blackbox[8] = {B11111,B11111,B11111,B11111,B11111,B11111,B11111,B11111};
-
-
-/* Define Parameters */
-
-
+byte arrow_up[8] = {B00100,B01110,B11111,B11111,B11111,B11111,B11111,B11111};
+byte arrow_down[8] = {B00100,B01110,B11111,B11111,B11111,B11111,B11111,B11111};
+byte arrow_right[8] = {B11000,B11100,B11110,B11111,B11111,B11110,B11100,B11000};
+byte arrow_left[8] = {B00100,B01110,B11111,B11111,B11111,B11111,B11111,B11111};
 /* Variables */
+/* Non-changable var */
 Keypad kp4x4  = Keypad(makeKeymap(kp4x4Keys), rowKp4x4Pin, colKp4x4Pin, KEYPAD_ROWS, KEYPAD_COLS);
 LiquidCrystal_I2C lcd(0x27,20,4);
 Servo brushServo, upDownServo;
+/* Changable var */
 int currentMenu = 0;
-
+/* SETUP */
 void setup() {
   /* Init Serial USB */ 
   Serial.begin(9600); Serial.println(F("Initialize System"));
@@ -51,21 +50,19 @@ void setup() {
   /* ServoMotor Setup */ 
   brushServo.attach(BRUSH_SERVO_PIN); upDownServo.attach(UP_DOWN_SERVO_PIN);
   /* LCD Setup */ 
-  lcd.init(); lcd.createChar(0, blackbox); lcd.backlight();
+  lcd.init(); lcd.createChar(0, blackbox); lcd.createChar(1, arrow_up); lcd.createChar(6, arrow_right); lcd.backlight();
   /* Analog Key Setup */ 
   pinMode(ANALOG_KEY_PIN,INPUT_PULLUP);
   /* */
   pinMode(RESET_BUT_PIN, INPUT);
+  /* Start operations */
   start();
-//  plate_rotate(1000, 0);
- 
 }
-
+/* Leave loop EMPTY */
 void loop() {
-//  plate_move(10, 0);
-//  plate_rotate(10, 0);
-  
+//  cw();
 }
+/* Operation functions */
 void start(){
 //  lcd_welcome_page();
 //  delay(3000);
@@ -89,6 +86,7 @@ void menu(){
     currentMenu = currentMenu%3;
   }
 }
+/* Template Drawing */
 void square(){
   plate_move(120, 1); delay(1000);
   plate_rotate(90, 1);delay(1000);
@@ -98,32 +96,44 @@ void square(){
   plate_rotate(90, 1);delay(1000);
   plate_move(120, 1);delay(1000);
 }
+/* Manuan Drawing */
 void manual(){
    manual_des();
-   String btn;
-   while(1){
-     btn = readAnalogKeypad();
-//     Serial.println(btn);
-     if(btn=="RIGHT"){
-      plate_rotate(10, 0);
-      
-     }else if(btn=="LEFT"){
-      plate_rotate(10, 1);
-//      delay(1000);
-     }else if(btn=="UP"){
-      plate_move(10, 1);
-//      delay(1000);
-     }else if(btn=="DOWN"){
-      plate_move(10, 0);
-//      delay(1000);
-     }
-   }
+   char keyPress;
+  while(1){
+    keyPress = readKeypad();
+    if(keyPress=='2') plate_move(10,1);//forward_driving();
+    if(keyPress=='4') plate_rotate(30,0);//ccw_driving();
+    if(keyPress=='6') plate_rotate(30,1);//cw_driving();
+    if(keyPress=='8') plate_move(10,0);//backward_driving();
+  }
+//   String btn;
+//   while(1){
+//     btn = readAnalogKeypad();
+////     Serial.println(btn);
+//     if(btn=="RIGHT"){
+//      plate_rotate(10, 0);
+//      
+//     }else if(btn=="LEFT"){
+//      plate_rotate(10, 1);
+////      delay(1000);
+//     }else if(btn=="UP"){
+//      plate_move(10, 1);
+////      delay(1000);
+//     }else if(btn=="DOWN"){
+//      plate_move(10, 0);
+////      delay(1000);
+//     }
+//   }
    
 }
+/* LCD Display */
 void manual_des(){
     lcd.clear();
     lcd.setCursor(0,0);
-    lcd.print("2| UP");
+    lcd.print("2| ");
+    lcd.setCursor(4,0);
+    lcd.write(byte(1));
     lcd.setCursor(0,1);
     lcd.print("4| LEFT");
     lcd.setCursor(0,2);
@@ -168,28 +178,36 @@ void menu3(){
 void plate_rotate(int degree, int clockwise){
   /* if clockwise = 1 will rotate >>> and if clockwise = 0 will rotate <<< */
   int steps = degree*STEPS_PER_DEGREE;
-  if(!clockwise){
-    digitalWrite(DIR_PIN_RIGHT, HIGH);  digitalWrite(DIR_PIN_LEFT, HIGH);
-  }else{
-    digitalWrite(DIR_PIN_RIGHT, LOW);  digitalWrite(DIR_PIN_LEFT, LOW);
-  }
   for(int i=0; i<steps;i++){
-    digitalWrite(STEP_PIN_RIGHT, HIGH); digitalWrite(STEP_PIN_LEFT, HIGH); delayMicroseconds(DELAY);
-    digitalWrite(STEP_PIN_RIGHT, LOW); digitalWrite(STEP_PIN_LEFT, LOW); delayMicroseconds(DELAY);  
+    clockwise==1?cw_driving():ccw_driving();
   }
 }
-void plate_move(int mm, int forward){
+void plate_move(int mm, int fw){
   /* forward can be only 1 or 0 */
   int steps = mm*STEPS_PER_MM;
-  if(!forward){
-    digitalWrite(DIR_PIN_RIGHT, HIGH); digitalWrite(DIR_PIN_LEFT, LOW);
-  }else{
-    digitalWrite(DIR_PIN_RIGHT, LOW); digitalWrite(DIR_PIN_LEFT, HIGH);
-  }
   for(int i=0; i<steps;i++){
-    digitalWrite(STEP_PIN_RIGHT, HIGH); digitalWrite(STEP_PIN_LEFT, HIGH); delayMicroseconds(DELAY);
-    digitalWrite(STEP_PIN_RIGHT, LOW); digitalWrite(STEP_PIN_LEFT, LOW); delayMicroseconds(DELAY);  
+    fw==1?forward_driving():backward_driving();
   }
+}
+void forward_driving(){
+  digitalWrite(DIR_PIN_RIGHT, LOW); digitalWrite(DIR_PIN_LEFT, HIGH);
+  driving();
+}
+void backward_driving(){
+  digitalWrite(DIR_PIN_RIGHT, HIGH); digitalWrite(DIR_PIN_LEFT, LOW);
+  driving();
+}
+void cw_driving(){
+  digitalWrite(DIR_PIN_RIGHT, HIGH);  digitalWrite(DIR_PIN_LEFT, HIGH);
+  driving();
+}
+void ccw_driving(){
+  digitalWrite(DIR_PIN_RIGHT, LOW);  digitalWrite(DIR_PIN_LEFT, LOW);
+  driving();
+}
+void driving(){
+  digitalWrite(STEP_PIN_RIGHT, HIGH); digitalWrite(STEP_PIN_LEFT, HIGH); delayMicroseconds(DELAY);
+  digitalWrite(STEP_PIN_RIGHT, LOW); digitalWrite(STEP_PIN_LEFT, LOW); delayMicroseconds(DELAY);  
 }
 /* Arm Functions */
 void reset_arm(){
